d650244f55e8af7f5d3722837895fcd31dd95897
EthanYuan <yuanchengxing@hotmail.com>
2024-10-15 21:41:46 +0800

    remove witness check

--- contracts/ckb-bitcoin-spv-type-lock/src/operations/reset.rs
+++ contracts/ckb-bitcoin-spv-type-lock/src/operations/reset.rs
@@ -1,9 +1,5 @@
-use ckb_bitcoin_spv_verifier::types::{
-    core::SpvTypeArgs,
-    packed::{SpvBootstrapReader, SpvClientReader, SpvInfoReader},
-    prelude::*,
-};
-use ckb_std::{ckb_constants::Source, debug, error::SysError, high_level as hl};
+use ckb_bitcoin_spv_verifier::types::{core::SpvTypeArgs, packed::SpvInfoReader, prelude::*};
+use ckb_std::{ckb_constants::Source, debug, high_level as hl};
 
 use crate::{
     error::{InternalError, Result},
@@ -15,17 +11,14 @@ pub(crate) fn reset_cells(
     outputs: &[usize],
     type_args: SpvTypeArgs,
 ) -> Result<()> {
-    if outputs.len() < 1 + 1 + 2 {
-        return Err(InternalError::CreateNotEnoughCells.into());
-    }
-    if inputs.windows(2).any(|pair| pair[0] + 1 != pair[1]) {
+    if inputs.windows(2).any(|pair| pair[1] + 1 != pair[0]) {
         return Err(InternalError::CreateShouldBeOrdered.into());
     }
-    if outputs.windows(2).any(|pair| pair[0] + 1 != pair[1]) {
+    if outputs.windows(2).any(|pair| pair[1] + 1 != pair[0]) {
         return Err(InternalError::CreateShouldBeOrdered.into());
     }
     // Checks args of the client type script, then returns the clients count;
-    let clients_count = {
+    let _clients_count = {
         let clients_count = usize::from(type_args.clients_count);
         let cells_count = 1 + clients_count;
         if outputs.len() != cells_count {
@@ -38,7 +31,7 @@ pub(crate) fn reset_cells(
         clients_count
     };
     // First cell is the client info cell.
-    let mut index = outputs[0];
+    let index = outputs[0];
     {
         debug!("check client info cell (index={index})");
         let output_data = hl::load_cell_data(index, Source::Output)?;
@@ -49,35 +42,6 @@ pub(crate) fn reset_cells(
         if info.tip_client_id != 0 {
             return Err(InternalError::CreateInfoIndexShouldBeZero.into());
         }
-    }
-    // Gets the client bootstrap from the witness.
-    let bootstrap = {
-        let witness_args = hl::load_witness_args(index, Source::Output)?;
-        if let Some(args) = witness_args.output_type().to_opt() {
-            SpvBootstrapReader::from_slice(&args.raw_data())
-                .map_err(|_| SysError::Encoding)?
-                .to_entity()
-        } else {
-            return Err(InternalError::CreateWitnessIsNotExisted.into());
-        }
-    };
-    // Gets the new client from the client bootstrap.
-    let mut expected_client = bootstrap.initialize_spv_client()?;
-    debug!("expected client cell (id=0): {}", expected_client.pack());
-    // Next `clients_count` cells are the client cells;
-    index += 1;
-    for _id in 0..clients_count {
-        debug!("check client cell (index={index}, id={_id})");
-        let output_data = hl::load_cell_data(index, Source::Output)?;
-        let actual = SpvClientReader::from_slice(&output_data)
-            .map_err(|_| InternalError::CreateBadClientCellData)?;
-        debug!("actual client cell: {actual}");
-        let expected = expected_client.pack();
-        if actual.as_slice() != expected.as_slice() {
-            return Err(InternalError::CreateNewClientIsIncorrect.into());
-        }
-        expected_client.id += 1;
-        index += 1;
     }
 
     Ok(())
